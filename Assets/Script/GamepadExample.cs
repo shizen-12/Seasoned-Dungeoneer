44f3cc95bc1f0266f3c7c68fdb50ec0561ab001f
shizen-12 <katudon4649_2@hotmail.com>
2024-01-07 16:54:04 +0900

    コントローラーで移動制御仮実装

---
+++ Assets/Script/GamepadExample.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GamepadExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // // ゲームパッドが接続されていないとnullになる。
+        // if (Gamepad.current == null) return;
+
+        // if (Gamepad.current.buttonNorth.wasPressedThisFrame)
+        // {
+        //     Debug.Log("Button Northが押された！");
+        // }
+        // if (Gamepad.current.buttonSouth.wasReleasedThisFrame)
+        // {
+        //     Debug.Log("Button Southが離された！");
+        // }
+    }
+    void OnGUI()
+    {
+        if (Gamepad.current == null) return;
+
+        GUILayout.Label($"leftStick: {Gamepad.current.leftStick.ReadValue()}");
+        GUILayout.Label($"rightStick: {Gamepad.current.rightStick.ReadValue()}");
+        GUILayout.Label($"dpad: {Gamepad.current.dpad.ReadValue()}");
+        // Dpadの上下左右の入力状態をBool型で取得
+        GUILayout.Label($"dpad up: {Gamepad.current.dpad.up.isPressed}");
+        GUILayout.Label($"dpad down: {Gamepad.current.dpad.down.isPressed}");
+        GUILayout.Label($"dpad left: {Gamepad.current.dpad.left.isPressed}");
+        GUILayout.Label($"dpad right: {Gamepad.current.dpad.right.isPressed}");
+
+        // // Dpadの上下左右の入力状態をfloat型で取得
+        // GUILayout.Label($"dpad up value: {Gamepad.current.dpad.up.ReadValue()}");
+        // GUILayout.Label($"dpad down value: {Gamepad.current.dpad.down.ReadValue()}");
+        // GUILayout.Label($"dpad left value: {Gamepad.current.dpad.left.ReadValue()}");
+        // GUILayout.Label($"dpad right value: {Gamepad.current.dpad.right.ReadValue()}");
+
+        GUILayout.Label($"buttonNorth (Y): {Gamepad.current.buttonNorth.isPressed}");
+        GUILayout.Label($"buttonSouth (A): {Gamepad.current.buttonSouth.isPressed}");
+        GUILayout.Label($"buttonEast (B): {Gamepad.current.buttonEast.isPressed}");
+        GUILayout.Label($"buttonWest (X): {Gamepad.current.buttonWest.isPressed}");
+        GUILayout.Label($"leftShoulder (LB): {Gamepad.current.leftShoulder.ReadValue()}");
+        GUILayout.Label($"rightShoulder (RB): {Gamepad.current.rightShoulder.ReadValue()}");
+        GUILayout.Label($"leftTrigger (LT): {Gamepad.current.leftTrigger.ReadValue()}");
+        GUILayout.Label($"rightTrigger (RT): {Gamepad.current.rightTrigger.ReadValue()}");
+        GUILayout.Label($"startButton: {Gamepad.current.startButton.isPressed}");
+        GUILayout.Label($"selectButton: {Gamepad.current.selectButton.isPressed}");
+        GUILayout.Label($"leftStickButton (LS): {Gamepad.current.leftStickButton.isPressed}");
+        GUILayout.Label($"rightStickButton (RS): {Gamepad.current.rightStickButton.isPressed}");
+    }
+
+}
---
+++ Assets/Script/PlayerTest.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerTest : MonoBehaviour
+{
+    public float gravityPower = -30;
+    public float jumpForce = 2.0f;
+    public float speed = 10.0f;
+    private Vector2 move;
+    public bool jumpNow;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.current != null)
+        {
+            move = Gamepad.current.leftStick.ReadValue();
+        }
+        else
+        {
+            move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+    private void FixedUpdate()
+    {
+        Gravity();
+        Vector3 movement = new Vector3(move.x, 0.0f, move.y);
+        rb.AddForce(movement * speed);
+
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 moveDirection = cameraForward * move.y + Camera.main.transform.right * move.x;
+        rb.velocity = moveDirection * speed + new Vector3(0, rb.velocity.y, 0);
+
+        if (moveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.phase);     //フェーズの表示を追加
+        // Performedフェーズの判定を行う
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("ジャンプ入力を受け付けました");
+            if (jumpNow == false)
+            {
+                rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+                jumpNow = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (jumpNow == true)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                jumpNow = false;
+            }
+        }
+    }
+    void Gravity()
+    {
+        if (jumpNow == true)
+        {
+            rb.AddForce(new Vector3(0, gravityPower, 0));
+        }
+    }
+}
